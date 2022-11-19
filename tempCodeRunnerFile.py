07c470ac1f67370104d4d04e57f931fe89dc559e
Goldragon <16520417@std.stei.itb.ac.id>
2022-11-20 01:04:04 +0700

    .

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+#Product Name
+        productNameFrame = Frame(LeftFrame, height=500, bg='powder blue')
+        productNameFrame.grid(row=0, column=0)
+        productName = Label(productNameFrame, width=35, font=('arial', 20, 'bold'), text='Barang A', bg='powder blue')
+        productName.pack(padx=20, pady=20, fill=BOTH)
