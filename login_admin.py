f4ae834e1ec46d567ed936c5226b139507a2693c
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2022-11-20 16:21:13 +0700

    benerin path gambar

--- login_admin.py
+++ login_admin.py
@@ -23,7 +23,7 @@ def login(root):
     cancel_button.pack()
 
 
-def showImg(file):
+def showImg(root, file):
     from PIL import ImageTk,Image    
     canvas = Canvas(root, width = 600, height = 600)  
     canvas.pack()  
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from PIL import ImageTk,Image
 
  
-#============================KONFIGURASI============================
+#============================KONFIGURASI============================#
 root = Tk() 
 root.configure(background='white')
 width= root.winfo_screenwidth()               
@@ -13,9 +13,25 @@ root.geometry("%dx%d+0+0" % (width, height))
 
 
 #DESIGN
-lable_title = Label(root, text="rocery Store Search Engine", font=("Product Sans", 30, 'bold'), bg='white')
-lable_title.pack()
-lable_title.place(x = 525 , y = 220)
+#===========================COLORPALETTE============================#
+#EA4335 RED
+#FBBC05 YELLOW
+#34A853 GREEN
+#4285F4 BLUE
+
+Title = Text(root, font=("Product Sans", 30, 'bold'), bg='white', bd=0, highlightthickness=0)
+Title.insert(INSERT, "rocery Store Search Engine")
+Title.pack()
+Title.place(x = 525 , y = 220)
+#Configure the text widget with certain color
+Title.tag_config("blue", foreground="#4285F4")
+Title.tag_config("green", foreground="#34A853")
+Title.tag_config("yellow", foreground="#FBBC05")
+Title.tag_config("red", foreground="#EA4335")
+Title.tag_add("blue", "1.0", "1.6")
+Title.tag_add("green", "1.7", "1.12") 
+Title.tag_add("yellow", "1.13", "1.19")
+Title.tag_add("red", "1.20", "1.26")
 
 canvas_logo = Canvas(root, width = 100, height = 100, highlightthickness=0)
 canvas_logo.pack()
@@ -26,13 +42,13 @@ Search_Bar = Entry(root, width = 31, font=('Product Sans',20), bg='white')
 Search_Bar.pack()
 Search_Bar.place(x = 440, y = 340)
 
-logo_source = Image.open("C:/Users/Thomas Stefen M/Dropbox/My PC (LAPTOP-VVOKBOQQ)/Documents/Python Scripts/TubesRPL_IF3152/logo.png")
+logo_source = Image.open("./logo.png")
 logo_source = logo_source.resize((100,100))
 
-searchImg_source = Image.open("C:/Users/Thomas Stefen M/Dropbox/My PC (LAPTOP-VVOKBOQQ)/Documents/Python Scripts/TubesRPL_IF3152/search.jpg")
+searchImg_source = Image.open("./search.jpg")
 searchImg_source = searchImg_source.resize((30,30))
 
-tambahBarang_source = Image.open("C:/Users/Thomas Stefen M/Dropbox/My PC (LAPTOP-VVOKBOQQ)/Documents/Python Scripts/TubesRPL_IF3152/TambahBarang_Button.png")
+tambahBarang_source = Image.open("./TambahBarang_Button.png")
 tambahBarang_source = tambahBarang_source.resize((30,30))
 
 logo = ImageTk.PhotoImage(logo_source)
@@ -42,10 +58,10 @@ tambahBarangImg = ImageTk.PhotoImage(tambahBarang_source)
 canvas_logo.create_image(1, 1, anchor = NW, image=logo)
 
 search_button = Button(root, image = searchImg, bd = 0, highlightthickness=0)
-search_button.place(x = 980, y = 345)
+search_button.place(x = 980, y = 344)
 
 tambahBarang_button = Button(root, image = tambahBarangImg, bd = 0, highlightthickness=0)
-tambahBarang_button.place(x = 1020, y = 345)
+tambahBarang_button.place(x = 1020, y = 344)
 
 #login(root)
 #root.withdraw() #This hides the main window, it's still present it just can't be seen or interacted with
